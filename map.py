05a710dc09288955ee755b9a54546cf6c274f201
VineeS <vinee@Vinees-MBP.fios-router.home>
2019-11-19 16:36:05 -0500

    with population

--- map.py
+++ map.py
@@ -26,10 +26,15 @@ for lt,ln,el  in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location= [lt,ln],radius= 6, popup= str(el)+ "m", 
     fill_color= color_produce(el), color = "grey", fill_capicity = 0.7))
 
-fg.add_child(folium.GeoJson(data = (open("/Users/vinee/Documents/workspace/code python/UdemyCourse/Mapping/world.json",
-"r", encoding = 'utf-8-sig').read())))
+# for population
+fg.add_child(folium.GeoJson(data = open("/Users/vinee/Documents/workspace/code python/UdemyCourse/Mapping/world.json",
+"r", encoding = 'utf-8-sig').read(), 
+style_function = lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10000000 
+else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
+
 
 map.add_child(fg)  
 map.save("Map2.html")
+
 
 
